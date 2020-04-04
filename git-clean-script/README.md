802a92544a1ec68b75f7d8e237627d67b953f6d0
Juan Carrey <juankarrey@gmail.com>
2020-04-04 11:55:23 +0200

    PR Feedback: Create options -l -v -h -r and make author a param

--- git-clean-script/README.md
+++ git-clean-script/README.md
@@ -6,9 +6,13 @@ This script is useful for keeping your local git repositories clean
 
 Add an alias to git (~/.gitconfig) Make sure your route is correct
 
+```
 [alias]
 	cln = "! /c/projects/utils/git-clean-script/git-clean.sh"
-	
+```	
+
 Execute with git in any git repository:
 
+```
 git cln
+```
--- git-clean-script/git-clean.sh
+++ git-clean-script/git-clean.sh
@@ -1,5 +1,40 @@
 #!/usr/bin/env bash
 
+# TODO Parse params in a better way....
+DEL_LOCAL=$(([ "$2" = "-l" ] || [ "$3" == "-l" ] || [ "$4" == "-l" ] || [ "$5" == "-l" ]) && echo "YES" || echo "NO")
+DEL_REMOTE=$(([ "$2" = "-r" ] || [ "$3" == "-r" ] || [ "$4" == "-r" ] || [ "$5" == "-r" ]) && echo "YES" || echo "NO")
+VERBOSE=$(([ "$2" = "-v" ] || [ "$3" == "-v" ] || [ "$4" == "-v" ] || [ "$5" == "-v" ]) && echo "YES" || echo "NO")
+SHOW_HELP=$(([ "$1" = "-h" ] || [ "$2" = "-h" ] || [ "$3" == "-h" ] || [ "$4" == "-h" ] || [ "$5" == "-h" ]) && echo "YES" || echo "NO")
+
+if [ "$SHOW_HELP" = "YES" ]; then
+    echo ""
+    echo "=============================="
+    echo "    Available Options"
+    echo "=============================="
+    echo ""
+    echo " -v    : Verbose: Displays commands to remove remote not merged branches "
+    echo " -l    : Remove Local: Deletes local merged branches otherwise dry-run "
+    echo " -r    : Remove remote: Deletes merged remotes branches, otherwise dry-run "
+    echo " -h    : Displays the help "
+    echo ""
+    echo ""
+    echo "=============================="
+    echo " Sample usages: "
+    echo "=============================="
+    echo ""
+    echo " ./git-clean.sh \"Juan Carrey\" -v "
+    echo " ./git-clean.sh \"Juan Carrey\" -v -l -r "
+    echo " ./git-clean.sh -h "
+    echo ""
+    echo ""
+    exit 0
+fi
+
+if [ "" == "$1" ]; then
+    echo " Author required for filtering remote branches "
+    exit 1
+fi
+
 git checkout master  > /dev/null 2>&1
 git fetch -p  > /dev/null 2>&1
 
@@ -14,8 +49,13 @@ echo ""
 
 for branch in `git branch --merged | grep -vwE "(\*|master|release|rc-.*)"`; 
 do 
-  echo -e `git show --format="Deleting %ci %cr %an" $branch | head -n 1` \\t$branch; 
-  echo -e `git branch -d $branch` \\t$branch; 
+  echo -e `git show --format="Deleting %ci %cr %an" $branch | head -n 1` $branch;
+
+  if [ "$DEL_LOCAL" = "YES" ]; then
+    echo -e `git branch -d $branch` $branch;
+  else
+    echo `git branch -d $branch` $branch;
+  fi
 done
 
 echo ""
@@ -29,8 +69,11 @@ echo ""
 
 for branch in `git branch --no-merged | grep -vwE "(\*|master|release|rc-.*)"`; 
 do 
-  echo -e `git show --format="· Are you still working on? %ci %cr %an" $branch | head -n 1` \\t$branch; 
-  # echo "git branch -D $branch"
+  echo -e `git show --format="· Are you still working on? %ci %cr %an" $branch | head -n 1` $branch;
+
+  if [ "$VERBOSE" = "YES" ]; then
+    echo "git branch -D $branch"
+  fi
 done | sort -r
   
 echo ""
@@ -41,21 +84,34 @@ echo "======================================"
 echo ""
 echo ""
 echo ""
-  
-  
 
 for branch in `git branch -r --merged | grep -v HEAD | grep -vwE "(\*|master|release|rc-.*)"`; do 
-  #author="git show --format=\"%an\" $branch | head -n 1"
-  #authorName=`eval $author`
-  #if [[ $authorName == "Juan Carrey" ]]
-  #then
-	echo -e `git show --format="Merged remote %ci %cr %an" $branch | head -n 1` \\t$branch;  
-    echo "git push --delete origin $branch"
-  #fi
+  author="git show --format=\"%an\" $branch | head -n 1"
+  authorName=`eval $author`
+  if [[ $authorName == $1 ]]
+  then
+    if [ "$DEL_REMOTE" = "YES" ]; then
+	    echo -e `git show --format="Merged remote %ci %cr %an" $branch | head -n 1` $branch;
+        echo -e "git push --delete origin $branch"
+    else
+        echo `git show --format="Merged remote %ci %cr %an" $branch | head -n 1` $branch;
+
+        if [ "$VERBOSE" = "YES" ]; then
+            echo "git push --delete origin $branch"
+        fi
+    fi
+
+  else
+  	if [ "$VERBOSE" = "YES" ]; then
+        echo `git show --format="Merged remote %ci %cr %an" $branch | head -n 1` $branch;
+        echo "git push --delete origin $branch"
+    fi
+  fi
 done
 
-
-git remote prune origin
+if [ "$DEL_REMOTE" = "YES" ]; then
+    git remote prune origin
+fi
 
 echo ""
 echo ""

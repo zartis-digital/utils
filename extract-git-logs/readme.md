6bff5e405aa6e7e9875f7a9a98a36b8b4f2eafe7
Alberto Suarez <alberto.suarez.caballero@gmail.com>
2021-04-12 19:07:28 +0200

    cleanup

--- extract-git-logs/readme.md
+++ extract-git-logs/readme.md
@@ -1,15 +1,18 @@
 # INSTRUCTIONS
 
-1. Duplicate run template file `run-extract-logs.temlpate.ps1` so it follows the name `run-extract-logs.local.ps1`
+1. Duplicate run template file `run-extract-logs.temlpate.ps1` and rename that copy to `run-extract-logs.local.ps1`
 2. Update `run-extract-logs.local.ps1` with the required parameters for your scenario.
-3. Run `run-extract-logs.local.ps1` ( out of windows, you probally need to run `pwsh` first)
+3. Run `run-extract-logs.local.ps1`
+
+   > out of windows, you probally need to run `pwsh` first
+
 4. Logs will be saved on the `temp` folder, organized in folders by year/date
 
 ## NOTES
 
 ### Dependecies
 
-> Non-windows machines will need the powershell core module installed on their computer ( compatible with linux / mac )
+> Non-windows machines will need the [powershell core module installed](https://docs.microsoft.com/en-us/powershell/scripting/install/installing-powershell?view=powershell-7.1) on their computer ( compatible with linux / mac )
 
 ### Getting the author name:
 
--- extract-git-logs/run-extract-logs.template.ps1
+++ extract-git-logs/run-extract-logs.template.ps1
@@ -2,11 +2,11 @@
 # => Duplicate this file so it follows the name `run-extract-logs.local.ps1`. Run this new file with the required parameters
 
 $fnParams = @{
-    gitAuthor         = "" # use when you want to start with a specific user ( app will promt u otherwise )
+    #gitAuthor         = "" # use when you want to start with a specific user ( app will promt u otherwise )
     targetBranch      = "master"   # look for logs on this branch
-    semester          = -1  # available values: 0 (full year ) 1 ( 1st semester ) 2 ( 2nd semester ). ( app will promt u otherwise )
-    targetYear        = "" # define target year( app will promt u otherwise )
-    nestedFolders     = 2 # define how many nested folder level the script should analysis ( app will use "4" as default )
+    #semester          = -1  # available values: 0 (full year ) 1 ( 1st semester ) 2 ( 2nd semester ). ( app will promt u otherwise )
+    #targetYear        = "" # define target year( app will promt u otherwise )
+    #nestedFolders     = 2 # define how many nested folder level the script should analysis ( app will use "4" as default )
     allProjectsFolder = "" # put the full path of the folder where your working git repos live in
     # for instace: "C:\Users\[user]\Documents\vscode"
     # or: "/home/[user]/vscode"

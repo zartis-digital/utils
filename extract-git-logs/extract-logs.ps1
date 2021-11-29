69967c03a252ec7e1064c97d4d77eb481cecf185
Jorge Luis Calderín García <JCalderin@globalshares.com>
2021-11-29 12:23:38 +0100

    Use Join-Path to combine base path and repository folder.

--- extract-git-logs/extract-logs.ps1
+++ extract-git-logs/extract-logs.ps1
@@ -87,7 +87,7 @@ try {
     $gitRepos = Get-ChildItem -recurse -Directory -Path $allProjectsFolder -Force -depth $nestedFolders -ErrorAction stop | Where-Object { $_.name -eq ".git" }
     write-host "Extracting logs.." -ForegroundColor DarkYellow -NoNewline
     foreach ($gitrepo in $gitRepos) {
-        $gitrepoFolderPath = "$($allProjectsFolder)/$($gitrepo.Parent)"
+        $gitrepoFolderPath = Join-Path -Path $allProjectsFolder -childPath $gitrepo.Parent
         $gitrepoName = $(get-item -path $gitrepoFolderPath ).name
         Set-Location -Path $gitrepoFolderPath -ErrorAction Stop > $null
         $gitlogfilename = "{0}-{1}.csv" -f $gitrepoName, $($gitAuthor -replace " ", "")
